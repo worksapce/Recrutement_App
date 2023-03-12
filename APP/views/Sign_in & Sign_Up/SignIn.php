bfc863b5bc443e56a4f80e21c9e1e6bfe243f445
J-oussama <91092592+J-oussama@users.noreply.github.com>
2023-03-12 00:32:43 +0000

    fixing bugs

--- APP/views/Sign_in & Sign_Up/SignIn.php
+++ APP/views/Sign_in & Sign_Up/SignIn.php
@@ -1,3 +1,9 @@
+<?php    	session_start();
+        if(isset($_SESSION['user'])){
+              header('Location: ./../testHome.php');
+              exit;
+        }
+?> 
 <!DOCTYPE html>
 <html lang="en">
 
